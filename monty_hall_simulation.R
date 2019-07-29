060dd8366ad31ca45ee8a41b98f5686f7037f56c
Aurelien Dugourd <Aurelien@Aureliens-Macbook.local>
2019-07-29 16:28:37 +0200

    make comments more readable

--- monty_hall_simulation.R
+++ monty_hall_simulation.R
@@ -13,24 +13,37 @@ for(i in 1:niter)
   
   door_numbers <- c(1,2,3) #We assign numbers to door, it's just to make it easier to manipulate
   
-  door_choice <- sample(door_numbers, size = 1) #We simulate the random choice of the door. We have 1/3 chances of picking the 1000000 dollar door at this point, and 2/3 chance of being wrong.
+  door_choice <- sample(door_numbers, size = 1) #We simulate the random choice of the door. We have 1/3 chances of 
+  #picking the 1000000 dollar door at this point, and 2/3 chance of being wrong.
   
-  if(doors[door_choice] != prices[1]) #Now we have two possible case scenario. First, what happens if i didn't pick the 1000000 dollar door? This scenario happens in 2 out of 3 cases.
+  if(doors[door_choice] != prices[1]) #Now we have two possible case scenario. First, what happens if i didn't pick 
+  #the 1000000 dollar door? This scenario happens in 2 out of 3 cases.
   {
-    show_door_0 <- door_numbers[door_numbers != door_choice & doors != prices[1]] #Then the host doesn't have a choice and have to show the other door (beside the one we picked) that has no money behind.
-    #By doing so, the host is essentially revealing the 1000000 dollar door, because it's the last remaining door, beside our first choice and the one he just revealed.
+    show_door_0 <- door_numbers[door_numbers != door_choice & doors != prices[1]] #Then the host doesn't have a choice
+    #and have to show the other door (beside the one we picked) that has no money behind.
     
-  } else #The second case, which happens in 1/3 cases, the host can simply show one of the two remaing door with no price behind.
+    #By doing so, the host is essentially revealing the 1000000 dollar door, because it's the last remaining door, 
+    #beside our first choice and the one he just revealed.
+    
+  } else #The second case, which happens in 1/3 cases, the host can simply show one of the two remaing door with no 
+    #price behind.
   {
-    show_door_0 <- door_numbers[sample(door_numbers[doors != prices[1]], 1)] #show randomly one of the two remaing loosing doors.
-    #This case happens only with 1/3 chances. Here, the host didn't reveal the the right door either, because both of the door he could have shown where loosing door anyway.
+    show_door_0 <- door_numbers[sample(door_numbers[doors != prices[1]], 1)] #show randomly one of the two remaing
+    #loosing doors.
+    
+    #This case happens only with 1/3 chances. Here, the host didn't reveal the the right door either, because both 
+    #of the door he could have shown where loosing door anyway.
   }
   
-  new_door_choice <- door_numbers[door_numbers != door_choice & door_numbers != show_door_0] #This is the situation where we switch.
+  new_door_choice <- door_numbers[door_numbers != door_choice & door_numbers != show_door_0] #This is the situation
+  #where we switch.
   keep_first_door_choice <- door_choice #This is the situation where we stick to our first choice. 
   
-  #This is were the trick is. Remember that if we switch, the scenario where we loose is the one where we had picked the winning door in the first choice. This happens in only 1/3 cases.
-  #This means that we only loose by switching in 1/3 cases, not 1/2 !! Because in 2/3 of cases, we picked the wrong door, leading to scenario 1 where the host doesn't have a choice in which door he 
+  #This is were the trick is. Remember that if we switch, the scenario where we loose is the one where we had 
+  #picked the winning door in the first choice. This happens in only 1/3 cases.
+  
+  #This means that we only loose by switching in 1/3 cases, not 1/2 !! Because in 2/3 of cases, we picked the 
+  #wrong door, leading to scenario 1 where the host doesn't have a choice in which door he 
   #can show us, thereby indirectly revealing the 1000000 door !
   
   price_switch <- doors[new_door_choice] #gain if we switched
